dc4df4725887e6e510e305ff5ae7dbaa4e32c957
931447022 <931447022@qq.com>
2020-09-28 15:21:38 +0800

    轮播图

---
+++ pages/index/components/search.vue
@@ -0,0 +1,86 @@
+<template>
+  <view>
+    <!-- 搜索 -->
+    <view class="search-cont">
+      <view class="search-code">
+        <image src="../../../static/icon/erweima.svg" mode="widthFix"></image>
+      </view>
+      <view class="search">
+      <image src="../../../static/icon/ss.svg" mode="widthFix" class="search-img"></image>
+      <input type="text" placeholder="请输入文字" disabled>
+
+      </view>
+      <view class="search-code">
+        <image src="../../../static/icon/xiaoxi.svg" mode="widthFix"></image>
+      </view>
+    </view>
+    <!-- 轮播图 -->
+    <swiper  :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true" indicator-color="rgba(255,255,255)" indicator-active-color="#00ff00">
+      <swiper-item>
+        <view class="swiper-item" id="swiper-item" @click="localCont()"><image src="https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2579860314,778624796&fm=26&gp=0.jpg" mode="" /></view>
+      </swiper-item>
+      <swiper-item>
+        <view class="swiper-item" id="swiper-item" @click="localCont()"><image src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1601272129441&di=9484548ca30554996be8c1a0908a9e97&imgtype=0&src=http%3A%2F%2Fimg.improve-yourmemory.com%2Fpic%2Fd47d90903d43dcb4ec62f7f99bda574b-0.jpg" mode="" /></view>
+      </swiper-item>
+      <swiper-item>
+        <view class="swiper-item" id="swiper-item" @click="localCont()"><image src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1601272129440&di=84f4d1464e6b9dbbf4e2f00324ff6541&imgtype=0&src=http%3A%2F%2Fpic1.win4000.com%2Fwallpaper%2Ff%2F54e18e5654482.jpg" mode="" /></view>
+      </swiper-item>
+        </swiper>
+  </view>
+</template>
+
+<script>
+export default {
+  name: "search",
+};
+</script>
+
+<style lang="scss" scoped>
+.search-cont {
+  display: flex;
+  justify-content: space-between;
+  height: 120rpx;
+  align-items: center;
+  background: linear-gradient(to top,#ffe566 10%,#ffd300 100%);
+  padding: 30rpx 0;
+  .search-code {
+    width: 70rpx;
+    height: 70rpx;
+  }
+
+  .search {
+    height: 80rpx;
+    line-height: 80rpx;
+    width: 100%;
+    display: flex;
+    flex-direction:flex;
+    background: #fff7d4;
+    border-top-left-radius: 60rpx;
+    border-bottom-left-radius: 60rpx;
+    border-bottom-right-radius: 60rpx;
+    margin: 0 10rpx;
+    .search-img {
+      margin: auto 0 auto 20rpx;
+      width: 100rpx;
+      height: 100rpx;
+    }
+
+    input {
+      height: 100rpx;
+      line-height: 100rpx;
+      width: 100%;
+      font-size: 30rpx;
+      color: #666666;
+    }
+  }
+}
+//轮播
+.imageurl{
+  width: 100%;
+  height: 300rpx !important;
+  swiper{
+    height: 300rpx !important;
+
+  }
+}
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,52 +1,54 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
-	</view>
+  <view>
+    <!-- 搜索 开始 -->
+    <search></search>
+
+    <!-- 搜索 结束 -->
+
+    <view class="content">
+      <!-- 轮播图 开始 -->
+      <!-- <view class="swiper">
+        <swiper autoplay indicator-dots circular>
+        <swiper-item v-for="item in banner" :key="item.id">
+          <image :src="item.thumb"></image>
+        </swiper-item>
+        </swiper>
+      </view>-->
+      <!-- 轮播图 结束 -->
+
+      <!-- 热门推荐 开始 -->
+      <view class="recommend"></view>
+      <!-- 热门推荐 结束 -->
+    </view>
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
+import Search from "./components/search";
+export default {
+  components: {
+    Search,
+  },
+  data() {
+    return {};
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+.search {
+  height: 80rpx;
+  button {
+    font-size: 15px;
+    color: #808080;
+    border-radius: 80rpx;
+    display: flex;
+    height: 80rpx;
+    border: #808080;
+    margin: 20rpx 20rpx;
+    background-color: #f1f1f1;
+    justify-content: center;
+    align-items: center;
+  }
+}
 </style>
